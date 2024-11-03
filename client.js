eeb19c6ecee9ba3ce559b2bcb02d544e29806cf7
Chris Stone <chrisstonedev@gmail.com>
2024-11-03 12:31:00 -0500

    New initial commit for rewrite test

--- client.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from '@sanity/client';
-
-export default sanityClient({
-    projectId: 'xw35o1hi',
-    dataset: 'production',
-    apiVersion: '2021-12-21',
-    useCdn: false
-})
--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  images: {
-    domains: ['cdn.sanity.io'],
-  },
-  reactStrictMode: true,
-}
